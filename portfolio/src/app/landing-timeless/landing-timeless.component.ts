22de92a9a7c44984259e378b68d3b0be92bc2fcc
xyberstudio <xyberllc@gmail.com>
2024-02-16 10:38:59 -0600

    Commiting initial project files and inital AI gen code for timeless landing

---
+++ portfolio/src/app/landing-timeless/landing-timeless.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-timeless',
+  templateUrl: './landing-timeless.component.html',
+  styleUrls: ['./landing-timeless.component.css', '../../styles.css']
+})
+export class LandingTimelessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
